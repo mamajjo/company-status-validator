0c491245f01ac86fa06a79925a0fd4796443775f
Maciej Majchrowski <majchr@MajchrOSX.local>
2019-11-30 20:47:39 +0100

    fixed export problems and finalized first function

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const apiService = require("./lib/apiService");
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 
 
+clear();
+console.log(
+    chalk.yellow(
+        figlet.textSync('Nip validator', {horizontalLayout:'full'})
+    )
+);
+apiService.getStatusByNIP(7282635947);
