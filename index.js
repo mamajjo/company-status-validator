fb5cde42a7e5f35cb250ecf893643a65fb787694
Maciej Majchrowski <majchr@MajchrOSX.local>
2020-01-02 12:11:06 +0100

    added reactive table to program

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const apiService = require("./lib/apiService");
 const argumentCheckerService = require("./lib/argumentCheckerService");
-const questionService = require("./lib/inquirer");
-const chalk = require('chalk');
 const clear = require('clear');
-const figlet = require('figlet');
+const Tabulator = require('tabulator-tables');
 const nullArg = {
     _: []
 };
@@ -12,19 +10,14 @@ var argv = require('minimist')(process.argv.slice(2), {
 });
 
 clear();
-console.log(
-    chalk.yellow(
-        figlet.textSync('Nip validator', {
-            horizontalLayout: 'full'
-        })
-    )
-);
+let checkedArguments;
+let resultData = [];
 let inputBoxHolder = document.getElementById("input-box");
 let inputBoxElement = document.createElement("INPUT");
 let submitButtonHolder = document.getElementById("button-cell");
 let submitButtonElement = document.createElement("button");
 let unsuccesfulValidationNumbersHolder = document.getElementById("wrong-number-section")
-let unsuccesfulValidationNumbersElement = document.createTextNode("Podane numery nie są prawidłowe: ");
+let unsuccesfulValidationNumbersElement = document.createTextNode("");
 var paragraph = document.getElementById("p");
 inputBoxHolder.appendChild(inputBoxElement);
 inputBoxElement.setAttribute("type", "text");
@@ -32,28 +25,45 @@ inputBoxElement.setAttribute("id", "vat-credential");
 inputBoxElement.setAttribute("placeholder", "wprowadź liczby po spacji");
 submitButtonHolder.appendChild(submitButtonElement);
 submitButtonElement.setAttribute("class", "button");
+submitButtonElement.textContent = "Sprawdź numery!";
+let resultTable = new Tabulator("#result-table", {
+    height:"200px",
+    layout:"fitColumns",
+    data: resultData,
+    reactiveData: true,
+    columns:[
+        {title:"Nazwa", field:"name", width:300},
+        {title:"Numer", field:"id", align:"right"},
+        {title:"Status", field:"statusVat", align:"center"},
+    ]
+});
 submitButtonElement.addEventListener('click', function(click, inputString = inputBoxElement.value){
     let numbersToCheck = inputString.split(" ");
     console.log(numbersToCheck);
     if (JSON.stringify(numbersToCheck)) {
-        let checkedArguments = argumentCheckerService.checkGivenArguments(numbersToCheck);
-        unsuccesfulValidationNumbersElement.nodeValue += checkedArguments.NoValidationPass.join(", ").toString();
-        unsuccesfulValidationNumbersHolder.appendChild(unsuccesfulValidationNumbersElement);
+        checkedArguments = argumentCheckerService.checkGivenArguments(numbersToCheck);
+        if (checkedArguments.NoValidationPass.length > 0){
+            unsuccesfulValidationNumbersElement.nodeValue += ("Podane numery nie są prawidłowe: ");
+            unsuccesfulValidationNumbersElement.nodeValue += checkedArguments.NoValidationPass.join(", ").toString();
+            unsuccesfulValidationNumbersHolder.appendChild(unsuccesfulValidationNumbersElement);
+        }
         checkedArguments.NIPs.forEach(numberToCheck => {
             let toCheck = parseInt(numberToCheck);
             let response = apiService.getStatusByNIP(toCheck);
             response.then(mes => {
                 paragraph.textContent += JSON.stringify(mes);
+                resultData.push({name: mes.name, id: mes.id, statusVat: mes.statusVat})
             })
         });
         checkedArguments.BAs.forEach(numberToCheck => {
             let response = apiService.getStatusByBA(numberToCheck);
             response.then(mes => {
                 paragraph.textContent += JSON.stringify(mes);
+                resultData.push({name: mes.name, id: mes.id, statusVat: mes.statusVat});
             })
         })
         checkedArguments.WrongNumbers.forEach(wrongNumber => {
-            paragraph.textContent += "nieprawidłowy numer" + wrongNumber
+            paragraph.textContent += "Nieprawidłowy numer" + wrongNumber
         })
     } else {
         paragraph.textContent +="Podaj numery do sprawdzenia jako argumenty do programu";
